cc503740749bf49cc7caba0ab2b7b2a4d25d7b9c
DELL <DELL@DESKTOP-BHH4R6N>
2018-07-11 18:15:35 +0530

    @syed created C class and modified main method

---
+++ SampleGitPro/src/pack1/C.java
@@ -0,0 +1,15 @@
+package pack1;
+
+public class C {
+	
+	int a=20;
+	int b=30;
+	public void add() {
+		System.out.println(a+b);
+	}
+	public static void main(String[] args) {
+		C a=new C();
+		a.add();
+	}
+
+}
