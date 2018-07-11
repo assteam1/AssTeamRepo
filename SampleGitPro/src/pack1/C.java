825de4be060c5d0b714dbc0d51e1f20d6aed96e5
Krishna Animi <Krishna Animi@KrishnaAnimi>
2018-07-11 18:16:01 +0530

    @sathish added int c n added method mul() to classC

--- SampleGitPro/src/pack1/C.java
+++ SampleGitPro/src/pack1/C.java
@@ -4,8 +4,12 @@ public class C {
 	
 	int a=20;
 	int b=30;
+	int c=50;
 	public void add() {
 		System.out.println(a+b);
+	}
+	public void mul() {
+		System.out.println(a*b*c);
 	}
 	public static void main(String[] args) {
 		C a=new C();
